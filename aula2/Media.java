14f4bbed13b41b16e43319edbfec537ff8833823
Marina Jensen <84468498+marinacjensen@users.noreply.github.com>
2024-02-26 21:49:41 -0300

    first commit

---
+++ aula2/Media.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Media {
+    
+    public static void main (String[] args) {
+
+        // Scanner in;
+        // in = new Scanner(System.in);
+
+        System.out.println("funciona!");
+
+    }
+
+}
